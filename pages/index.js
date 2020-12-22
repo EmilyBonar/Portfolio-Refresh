d94d7e7f674df331a94fc1689436efd7722ea0ed
emilybonar <emilykbonar@gmail.com>
2020-12-21 19:09:39 -0600

    Added links

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import Head from "next/head";
 
 const Home = () => (
-	<div>
-		<h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+	<div className="flex flex-col">
+		<div className="flex w-4/5 m-auto gap-8 text-xl text-gray-900">
+			<a href="./">Home</a>
+			<a href="./software">Software</a>
+			<a href="./materials">Materials</a>
+		</div>
 
-		<h3>
-			Hooray 🎉 - you've built this with{" "}
-			<a href="https://nextjs.org">Next.js</a>!
-		</h3>
-		<a href="./software">Software</a>
-		<a href="./materials">Materials</a>
+		<div className="w-4/5 m-auto bg-gray-100 opacity-95 text-right p-8 rounded">
+			<h1 className="text-7xl mb-4">Emily Bonar</h1>
+			<h3 className="text-2xl text-gray-700">
+				Software Developer and Materials Scientist
+			</h3>
+		</div>
 	</div>
 );
 
