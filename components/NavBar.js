2499fb15bd7925ab932149e5d70fcf945862d6bb
emilybonar <emilykbonar@gmail.com>
2021-01-04 14:30:05 -0600

    Prettied up nav bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,21 @@
 export function NavBar(props) {
 	return (
 		<div className="flex w-5/6 lg:w-3/4 m-auto gap-8 text-xl text-gray-900 mt-8 mb-4">
-			<a href="./" className="mr-8">
-				Home
-			</a>
-			<a href="./projects" className="mr-8">
-				Projects
-			</a>
-			<a href="./resume" className="mr-8">
-				Resume
-			</a>
+			<NavItem name="Home" location="./" />
+			<NavItem name="Projects" location="./projects" />
+			<NavItem name="Resume" location="./resume" />
 		</div>
+	);
+}
+
+function NavItem(props) {
+	return (
+		<a
+			href={props.location}
+			className="mr-8 border-b-2 border-black hover:border-opacity-90 border-opacity-0 transition"
+		>
+			{props.name}
+		</a>
 	);
 }
 
