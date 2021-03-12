42762aee34917a56827f5fa8b49d113d02bcd93d
emilybonar <emilykbonar@gmail.com>
2021-03-12 08:31:57 -0600

    Align links on cards

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -6,7 +6,7 @@ export function ProjectCard(props) {
 	return (
 		<AnimatedCard
 			content={
-				<div className="flex flex-col min-h-full pb-8 m-auto overflow-hidden transition transform bg-white rounded-lg shadow-lg hover:-translate-y-2">
+				<div className="flex flex-col h-full min-h-full pb-8 m-auto overflow-hidden transition transform bg-white rounded-lg shadow-lg hover:-translate-y-2">
 					<a href={props.liveLink} target="_blank">
 						<img
 							src={props.media}
@@ -15,29 +15,30 @@ export function ProjectCard(props) {
 							width="256"
 						/>
 					</a>
-
-					<h1 className="px-8 pt-4 text-2xl font-bold tracking-wide text-gray-900">
-						{props.title}
-					</h1>
-					<p className="px-8 pt-1 text-xs tracking-widest text-gray-600 uppercase">
-						{props.technologies.join(" | ")}
-					</p>
-					<div className="px-8 pt-2 text-gray-900">{props.copy}</div>
-					<div className="flex m-auto mt-4">
-						{props.liveLink && (
-							<Logo
-								icon="/external-link.svg"
-								link={props.liveLink}
-								name="Live Site"
-							/>
-						)}
-						{props.gitLink && (
-							<Logo
-								icon="/GitHub-Mark.svg"
-								link={props.gitLink}
-								name="GitHub"
-							/>
-						)}
+					<div className="flex flex-col h-1/2">
+						<h2 className="px-8 pt-4 text-2xl font-bold tracking-wide text-gray-900">
+							{props.title}
+						</h2>
+						<p className="px-8 pt-1 text-xs tracking-widest text-gray-600 uppercase">
+							{props.technologies.join(" | ")}
+						</p>
+						<div className="h-full px-8 pt-2 text-gray-900">{props.copy}</div>
+						<div className="flex m-auto mt-4">
+							{props.liveLink && (
+								<Logo
+									icon="/external-link.svg"
+									link={props.liveLink}
+									name="Live Site"
+								/>
+							)}
+							{props.gitLink && (
+								<Logo
+									icon="/GitHub-Mark.svg"
+									link={props.gitLink}
+									name="GitHub"
+								/>
+							)}
+						</div>
 					</div>
 				</div>
 			}
