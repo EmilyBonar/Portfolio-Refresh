3b273109767685b7385cfdb108da9ffefe315814
emilybonar <emilykbonar@gmail.com>
2021-03-13 10:22:57 -0600

    Added search to projects

--- pages/projects.js
+++ pages/projects.js
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header.js";
 import projects from "../data/projectData.js";
 import ProjectCard from "../components/ProjectCard.js";
 import NavBar from "../components/NavBar.js";
 
-const Projects = () => (
-	<div className="flex flex-col w-5/6 gap-4 m-auto mb-4 max-w-7xl lg:w-3/4">
-		<Header title="Emily Bonar - Projects" />
-		<NavBar />
-		<main className="grid grid-flow-row-dense gap-4 m-auto md:grid-cols-2 xl:grid-cols-3">
-			{projects.map((project, index) => (
-				<ProjectCard
-					title={project.title}
-					media={project.media}
-					technologies={project.technologies}
-					copy={project.copy}
-					gitLink={project.gitLink}
-					liveLink={project.liveLink}
-					index={index}
-				/>
-			))}
-		</main>
-	</div>
-);
+function Projects() {
+	const [search, setSearch] = useState("");
+	return (
+		<div className="flex flex-col w-5/6 gap-4 m-auto mb-4 max-w-7xl lg:w-3/4">
+			<Header title="Emily Bonar - Projects" />
+			<NavBar />
+			<SearchBar onInput={(input) => setSearch(input)} />
+			<main className="grid grid-flow-row-dense gap-4 m-auto md:grid-cols-2 xl:grid-cols-3">
+				{projects
+					.filter(
+						(project) =>
+							project.title.toLowerCase().includes(search.toLowerCase()) ||
+							project.technologies
+								.join(" ")
+								.toLowerCase()
+								.includes(search.toLowerCase()) ||
+							project.copy.toLowerCase().includes(search.toLowerCase()),
+					)
+					.map((project, index) => (
+						<ProjectCard
+							title={project.title}
+							media={project.media}
+							technologies={project.technologies}
+							copy={project.copy}
+							gitLink={project.gitLink}
+							liveLink={project.liveLink}
+							index={index}
+						/>
+					))}
+			</main>
+		</div>
+	);
+}
+
+function SearchBar(props) {
+	return (
+		<input
+			className="p-3 text-xl rounded-full outline-none focus:ring-2 ring-gray-500"
+			placeholder="Search..."
+			onInput={(e) => props.onInput(e.target.value)}
+		></input>
+	);
+}
 
 export default Projects;
