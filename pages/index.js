27beaeaf1514de85761b79f361564f4d65f1f25e
emilybonar <emilykbonar@gmail.com>
2021-03-27 15:03:08 -0500

    Remove width rule

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/Header.js";
 import HomeCard from "../components/HomeCard.js";
 
 const Home = () => (
-	<div className="flex flex-col w-5/6 m-auto mb-4 max-w-7xl lg:w-3/4">
+	<div className="flex flex-col w-5/6 m-auto mb-4 max-w-7xl ">
 		<Header title="Emily Bonar" />
 		<NavBar />
 		<main>
