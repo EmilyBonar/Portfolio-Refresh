49d50a4c4ef177077e25b3de539ea26b2a7cd5cc
emilybonar <emilykbonar@gmail.com>
2021-04-01 16:23:31 -0500

    Make resume stay on screen

--- pages/resume.js
+++ pages/resume.js
@@ -3,10 +3,13 @@ import Header from "../components/Header.js";
 import NavBar from "../components/NavBar.js";
 
 const Resume = () => (
-	<div className="flex flex-col w-5/6 gap-4 m-auto mb-4 max-w-7xl">
+	<div className="flex flex-col w-5/6 max-h-screen gap-4 m-auto max-w-7xl">
 		<Header title="Emily Bonar - Resume" />
 		<NavBar />
-		<iframe src="/Emily Bonar Resume.pdf" className="w-full h-screen"></iframe>
+		<iframe
+			src="/Emily Bonar Resume.pdf"
+			className="w-full h-screen pb-4"
+		></iframe>
 	</div>
 );
 
